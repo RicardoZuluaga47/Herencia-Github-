4b6ad45e7c0801f294edba72f58191bae5d458c2
Ricardo Z <kleyric@gmail.com>
2026-08-27 20:45:32 -0500

    Encapsulamiento de atributos de la superclase Personaje y creación de métodos getters y setters

--- src/herencia/github/Personajes.java
+++ src/herencia/github/Personajes.java
@@ -1,7 +1,35 @@
 package herencia.github;
 public class Personajes {
-    double vida = 0;
-    int nivel = 0;
-    String nombre = "";
-    double daño = 0;
+    private double vida = 0;
+    private int nivel = 0;
+    private String nombre = "";
+    private double daño = 0;
+    
+    public double getVida(){
+        return vida;
+    }
+    public void setVida(double vida){
+        this.vida = vida;
+    }
+    
+    public int getNivel(){
+        return nivel;
+    }
+    public void setNivel(int nivel){
+        this.nivel = nivel;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public double getDaño(){
+        return daño;
+    }
+    public void setDaño(double daño){
+        this.daño = daño;
+    }
 }

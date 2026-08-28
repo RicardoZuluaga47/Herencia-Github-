e4ad7c27eb0e0513c3623fd11b98bdbfa4a4abfd
Ricardo Z <kleyric@gmail.com>
2026-08-27 21:44:49 -0500

    Instanciacion de objeto tipo NPCs: herrero y Asignacion de datos

--- src/herencia/github/HerenciaGithub.java
+++ src/herencia/github/HerenciaGithub.java
@@ -3,6 +3,7 @@ public class HerenciaGithub {
 public static void main(String[] args) {
     Jugador player = new Jugador();
     Enemigos enemigo1 = new Enemigos();
+    NPCs herrero = new NPCs();
     
     player.setNombre("Ricky_47");
     player.setClase("Barbaro");
@@ -19,5 +20,13 @@ public static void main(String[] args) {
     enemigo1.setVida(3000);
     enemigo1.setDaño(214.05);
     enemigo1.setRecompensa(1000);
+    
+    herrero.setNombre("Wilex");
+    herrero.setProfecion("Herrero");
+    herrero.setInteraccion(true);
+    herrero.setNivel(30);
+    herrero.setVida(200);
+    herrero.setDaño(32.41);
+    herrero.setAmistad(50);
     }
 }

2aafe56bdbf87af265d846bc72ca31d8643cb7f3
Ricardo Z <kleyric@gmail.com>
2026-08-27 21:10:03 -0500

    Encapsulamiento de atributos de la ClaseHija NPCs y creación de métodos getters y setters

--- src/herencia/github/NPCs.java
+++ src/herencia/github/NPCs.java
@@ -3,4 +3,25 @@ public class NPCs extends Personajes {
     String profecion = "";
     boolean interaccion;
     int amistad = 0;
+    
+    public int getAmistad(){
+        return amistad;
+    }
+    public void setAmistad(int amistad){
+        this.amistad = amistad;
+    }
+    
+    public String getProfecion(){
+        return profecion;
+    }
+    public void setProfecion(String profecion){
+        this.profecion = profecion;
+    }
+    
+    public boolean getInteraccion(){
+        return interaccion;
+    }
+    public void setInteraccion(boolean interaccion){
+        this.interaccion = interaccion;
+    }
 }

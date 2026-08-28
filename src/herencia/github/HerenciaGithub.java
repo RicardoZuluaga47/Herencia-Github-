f7547b7afa149621f55246f28076eecf1dd9a7c9
Ricardo Z <kleyric@gmail.com>
2026-08-27 21:34:16 -0500

    Instanciacion de objeto tipo Enemigos: enemigo1 y Asignacion de datos

--- src/herencia/github/HerenciaGithub.java
+++ src/herencia/github/HerenciaGithub.java
@@ -2,6 +2,7 @@ package herencia.github;
 public class HerenciaGithub {
 public static void main(String[] args) {
     Jugador player = new Jugador();
+    Enemigos enemigo1 = new Enemigos();
     
     player.setNombre("Ricky_47");
     player.setClase("Barbaro");
@@ -10,5 +11,13 @@ public static void main(String[] args) {
     player.setVida(451);
     player.setDaño(86.63);
     player.setDinero(1542);
+    
+    enemigo1.setNombre("Karvengor");
+    enemigo1.setRaza("Titan de lava");
+    enemigo1.setJefe(true);
+    enemigo1.setNivel(50);
+    enemigo1.setVida(3000);
+    enemigo1.setDaño(214.05);
+    enemigo1.setRecompensa(1000);
     }
 }

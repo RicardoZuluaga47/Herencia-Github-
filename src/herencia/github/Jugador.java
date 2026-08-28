27acfe242ae09d762075b9be2740cb3b0fbc320f
Ricardo Z <kleyric@gmail.com>
2026-08-27 21:06:05 -0500

    Encapsulamiento de atributos de la ClaseHija Jugador y creación de métodos getters y setters

--- src/herencia/github/Jugador.java
+++ src/herencia/github/Jugador.java
@@ -3,4 +3,25 @@ public class Jugador extends Personajes {
     double experiencia = 0;
     String clase = "";
     int dinero = 0;
+    
+    public double getExperiencia(){
+        return experiencia;
+    }
+    public void setExperiencia(double experiencia){
+        this.experiencia = experiencia;
+    }
+    
+    public int getDinero(){
+        return dinero;
+    }
+    public void setDinero(int dinero){
+        this.dinero = dinero;
+    }
+    
+    public String getClase(){
+        return clase;
+    }
+    public void setClase(String clase){
+        this.clase = clase;
+    }
 }

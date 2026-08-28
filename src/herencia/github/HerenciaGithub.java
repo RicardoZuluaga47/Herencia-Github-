ac6c6533720705f078ac17ef50fa7ce619dc746b
Ricardo Z <kleyric@gmail.com>
2026-08-27 21:24:47 -0500

    Instanciacion de objeto tipo Jugador: player y Asignacion de datos

--- src/herencia/github/HerenciaGithub.java
+++ src/herencia/github/HerenciaGithub.java
@@ -1,6 +1,14 @@
 package herencia.github;
 public class HerenciaGithub {
 public static void main(String[] args) {
+    Jugador player = new Jugador();
     
+    player.setNombre("Ricky_47");
+    player.setClase("Barbaro");
+    player.setNivel(24);
+    player.setExperiencia(395.57);
+    player.setVida(451);
+    player.setDaño(86.63);
+    player.setDinero(1542);
     }
 }

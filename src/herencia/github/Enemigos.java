6344b895e49f2d32c3ef3b36eca520747196d747
Ricardo Z <kleyric@gmail.com>
2026-08-27 21:02:12 -0500

    Encapsulamiento de atributos de la ClaseHija Enemigos y creación de métodos getters y setters

--- src/herencia/github/Enemigos.java
+++ src/herencia/github/Enemigos.java
@@ -1,6 +1,27 @@
 package herencia.github;
 public class Enemigos extends Personajes {
-    String raza = "";
-    boolean jefe;
-    int recompensa;
+    private String raza = "";
+    private boolean jefe;
+    private int recompensa = 0;
+    
+    public int getRecompensa(){
+        return recompensa;
+    }
+    public void setRecompensa(int recompensa){
+        this.recompensa = recompensa;
+    }
+    
+    public String getRaza(){
+        return raza;
+    }
+    public void setRaza(String raza){
+        this.raza = raza;
+    }
+    
+    public boolean getJefe(){
+        return jefe;
+    }
+    public void setJefe(boolean jefe){
+        this.jefe = jefe;
+    }
 }
